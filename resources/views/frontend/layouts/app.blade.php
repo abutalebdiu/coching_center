140726a22e60135d004b5f5fc4b556f23bf43b74
Abu <abutalebgmtt@gmail.com>
2021-06-07 18:13:51 +0600

    app edit

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -222,7 +222,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 wow animate__animated animate__fadeInUp">
-                    <p>2021 © All Rights Reserved. Powered by <a href="#">Domain Name</a></p>
+                    <p>2021 © All Rights Reserved. Powered by <a href="#">{{ $websetting->site_name }}</a></p>
                 </div>
             </div>
         </div>
